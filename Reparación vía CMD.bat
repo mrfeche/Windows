fee067749a8f4431d188e45fdedc1b1f27c420ff
Mr Feche <95451481+mrfeche@users.noreply.github.com>
2022-08-18 09:39:21 -0300

    Create Reparación vía CMD.bat

---
+++ Reparación vía CMD.bat
@@ -0,0 +1,6 @@
+
+DISM /Online /Cleanup-Image /CheckHealth
+DISM /Online /Cleanup-Image /ScanHealth
+DISM /Online /Cleanup-Image /RestoreHealth
+DISM /online /cleanup-image /startcomponentcleanup
+sfc /scannow
