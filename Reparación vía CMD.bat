1b8c77a563e21d614428276f7982d327791ba70f
Mr Feche <95451481+mrfeche@users.noreply.github.com>
2022-10-13 13:03:40 -0300

    Update Reparación vía CMD.bat

--- Reparación vía CMD.bat
+++ Reparación vía CMD.bat
@@ -1,6 +1,11 @@
-
+.
+echo on
+msg /time:3 * "Vamos a ejecutar comandos de reparación de Windows"
 DISM /Online /Cleanup-Image /CheckHealth
 DISM /Online /Cleanup-Image /ScanHealth
 DISM /Online /Cleanup-Image /RestoreHealth
+DISM /Online /Cleanup-Image /RestoreHealth /Source:"H:\CD" /LimitAccess
 DISM /online /cleanup-image /startcomponentcleanup
 sfc /scannow
+msg /time:2 * "Trabajo terminado!"
+msg /time:4 * "Si el problema persiste, volver a ejecutar en MODO SEGURO CON FUNCIONES DE RED"
